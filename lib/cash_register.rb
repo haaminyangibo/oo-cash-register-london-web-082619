43e00bf6e0f597c33c8b9de3f1e7abb8b0e6e065
Haami Nyangibo <haaminyangibo@gmail.com>
2019-08-28 14:29:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,11 @@
 class CashRegister
 
- attr_accessor :total 
+ attr_accessor :total, :discount, :last_trans
 
-  def initialize(total =0 )
-    @total = total
+  def initialize(discount = 0 )
+    @total = 0
+    @discount = discount 
+    @last
   end
  
  def add_item(title, price)
